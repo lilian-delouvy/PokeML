8285ee46d4f1a7236da6a8171ddb971f1f90f17f
Lilian Delouvy <lilian.delouvy@gmail.com>
2023-09-20 13:03:47 +0200

    feat(Front): add script to generate required assets for backend

--- pokeml-api/src/controller.py
+++ pokeml-api/src/controller.py
@@ -1,8 +1,14 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 
+# flask --app src/controller.py run
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello World !</p>"
+def base():
+    return send_from_directory('resources', 'index.html')
+
+
+@app.route("/<path:path>")
+def home(path):
+    return send_from_directory('resources', path)
